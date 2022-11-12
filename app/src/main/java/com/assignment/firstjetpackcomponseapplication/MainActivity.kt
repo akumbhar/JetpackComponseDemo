34ff223a686836b9b32625e69ced0d797e206a52
Aniket Kumbhar <akumbhar01@entrata,com>
2022-11-12 16:59:29 +0530

    Row and Column type linear layout

--- app/src/main/java/com/assignment/firstjetpackcomponseapplication/MainActivity.kt
+++ app/src/main/java/com/assignment/firstjetpackcomponseapplication/MainActivity.kt
@@ -3,13 +3,41 @@ package com.assignment.firstjetpackcomponseapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hi Pallavi!!")
+
+           /* Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Yellow),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly,
+                ) {
+
+                Text(text = "Hello!")
+                Text(text = "Jetpack Compose!!")
+            }*/
+
+            Row(modifier = Modifier.fillMaxSize().background(Color.Gray),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+                Text(text = "Hello!")
+                Text(text = "Hello!")
+                Text(text = "Hello!")
+
+            }
         }
     }
 }
